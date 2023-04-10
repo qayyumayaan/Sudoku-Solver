fc183d45e2e009a3f8633a801bf850c345272812
qayyumayaan <96453808+qayyumayaan@users.noreply.github.com>
2023-04-10 02:12:11 -0400

    Sudoku Guesser Can't Guess

--- sudokuAlg.py
+++ sudokuAlg.py
@@ -48,6 +48,50 @@ def initializeNumInts(sudokuM, numInts):
                 numInts[ int - 1 ] += 1  
                 
     return numInts
+
+def sudokuGuesser(grid, numGuesses):
+    stack = []
+    stack.append(grid)
+    
+    while len(stack) > 0 and numGuesses <= 0:
+        curr_grid = stack.pop()
+        empty_cell = find_empty_location(curr_grid)
+        if empty_cell is None:
+            return curr_grid
+        
+        row, col = empty_cell
+        for num in range(1, 10):
+            if is_valid_number(curr_grid, row, col, num):
+                new_grid = [row[:] for row in curr_grid]
+                new_grid[row][col] = num
+                stack.append(new_grid) 
+        numGuesses -= 1
+    grid = stack.pop()
+    print(grid)
+    return grid
+                
+def find_empty_location(grid):
+    """
+    This function finds an empty location in the Sudoku grid.
+    """
+    for row in range(9):
+        for col in range(9):
+            if grid[row][col] == 0:
+                return (row, col)
+    return None
+
+def is_valid_number(grid, row, col, num):
+    """
+    This function checks if a given number is valid for a given cell in the Sudoku grid.
+    """
+    for i in range(9):
+        if grid[row][i] == num:
+            return False
+        if grid[i][col] == num:
+            return False
+        if grid[(row // 3) * 3 + i // 3][(col // 3) * 3 + i % 3] == num:
+            return False
+    return True
     
 def findNextNum(numInts):
     maxI = 0
@@ -89,7 +133,14 @@ def mainRuntime(sudokuM):
     for i in range(9): numInts[i] = 0
 
     numInts, numOnBoardAtStart = initializeNumInts(sudokuM, numInts), counter(sudokuM)
-    maxIterations, boardSolved, totalAdded, numIterations = 1500, False, 0, 0 
+    maxIterations, boardSolved, totalAdded, numIterations = 15000, False, 0, 0 
+
+    # stack = []
+    # stack.append(sudokuM)
+    
+    numNoWrites = 0
+    thresholdForGuessing = 10
+    failedParity = 0
 
     for i in range(maxIterations):
         numToComplete = findNextNum(numInts)
@@ -103,6 +154,15 @@ def mainRuntime(sudokuM):
         
         totalAdded += numAddedThisIteration
         sudokuTemp = removeNegatives(sudokuTemp)
+
+        if (numNoWrites > thresholdForGuessing):
+            sudokuSavePoint = sudokuM
+            bruh = sudokuGuesser(sudokuM, 81)
+            print(bruh - sudokuUnModified)
+            numNoWrites = 0
+            # print(sudokuM)
+        elif numAddedThisIteration == 0:
+            numNoWrites += 1
         
         if (totalAdded + numOnBoardAtStart == 81): 
             numIterations, boardSolved = i, True
@@ -110,7 +170,12 @@ def mainRuntime(sudokuM):
         
         if (p.parityCheckFull(sudokuM) == False):
             sudokuM = sudokuSavePoint
+            failedParity += 1
         numIterations += 1
+        
+        if (numIterations % (maxIterations / 3) == 0): 
+            print("iterations: " + str(numIterations))
+            print("failed parity: " + str(failedParity))
 
     if (not boardSolved): print("\nWas unable to solve. Gave up after " + str(numIterations) + " iterations. ")
     else: print("\nSuccess! Solved after " + str(numIterations) + " iterations. ")
