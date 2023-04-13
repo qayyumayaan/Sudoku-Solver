5e3dc778bd8fb6f0a3e0690a7790783b7c25f13d
qayyumayaan <96453808+qayyumayaan@users.noreply.github.com>
2023-04-13 01:58:37 -0400

    Fully Functional Website

--- src/sudoku.js
+++ src/sudoku.js
@@ -1,3 +1,16 @@
+function parseInput() {
+  const board = [];
+  for (let i = 1; i <= 9; i++) {
+    const row = [];
+    for (let j = 1; j <= 9; j++) {
+      const cell = document.getElementsByName(`cell-${i}-${j}`)[0];
+      row.push(cell.value ? parseInt(cell.value) : 0);
+    }
+    board.push(row);
+  }
+  return board;
+}
+
 function findEmptyLocation(grid) {
     for (let row = 0; row < 9; row++) {
       for (let col = 0; col < 9; col++) {
@@ -9,55 +22,70 @@ function findEmptyLocation(grid) {
     return null;
   }
   
-  function isValid(grid, row, col, num) {
-    for (let i = 0; i < 9; i++) {
-      if (grid[row][i] === num) {
-        return false;
-      }
-      if (grid[i][col] === num) {
-        return false;
-      }
-      if (grid[Math.floor(row / 3) * 3 + Math.floor(i / 3)][Math.floor(col / 3) * 3 + (i % 3)] === num) {
-        return false;
-      }
+function isValid(grid, row, col, num) {
+  for (let i = 0; i < 9; i++) {
+    if (grid[row][i] === num) {
+      return false;
     }
+    if (grid[i][col] === num) {
+      return false;
+    }
+    if (grid[Math.floor(row / 3) * 3 + Math.floor(i / 3)][Math.floor(col / 3) * 3 + (i % 3)] === num) {
+      return false;
+    }
+  }
+  return true;
+}
+  
+function solve(grid) {
+  const emptyLoc = findEmptyLocation(grid);
+  if (!emptyLoc) {
     return true;
   }
-  
-  function solve(grid) {
-    const emptyLoc = findEmptyLocation(grid);
-    if (!emptyLoc) {
-      return true;
-    }
-    const [row, col] = emptyLoc;
-    for (let num = 1; num <= 9; num++) {
-      if (isValid(grid, row, col, num)) {
-        grid[row][col] = num;
-        if (solve(grid)) {
-          return true;
-        }
-        grid[row][col] = 0;
+  const [row, col] = emptyLoc;
+  for (let num = 1; num <= 9; num++) {
+    if (isValid(grid, row, col, num)) {
+      grid[row][col] = num;
+      if (solve(grid)) {
+        return true;
       }
+      grid[row][col] = 0;
     }
-    return false;
+  }
+  return false;
+}
+
+function solveSudoku() {
+
+  const inputTable = document.getElementsByTagName("table")[0];
+  const inputRows = inputTable.getElementsByTagName("tr");
+  const inputValues = [];
+  for (let i = 0; i < inputRows.length; i++) {
+    const inputCols = inputRows[i].getElementsByTagName("td");
+    const rowValues = [];
+    for (let j = 0; j < inputCols.length; j++) {
+      item = parseInt(inputCols[j].firstChild.value);
+      if (isNaN(item)) item = 0;
+      rowValues.push(item);
+    }
+    inputValues.push(rowValues);
   }
 
+  solve(inputValues);
+  print(inputValues);
+  document.getElementById("resultHeader").style.display = "block";
+}
 
-const grid = [
-    [	0	,	0	,	0	,	0	,	0	,	4	,	0	,	5	,	2	],
-    [	6	,	0	,	0	,	0	,	0	,	0	,	0	,	0	,	0	],
-    [	9	,	0	,	5	,	0	,	2	,	0	,	0	,	3	,	0	],
-    [	0	,	0	,	0	,	8	,	0	,	0	,	7	,	0	,	0	],
-    [	2	,	0	,	3	,	0	,	4	,	0	,	0	,	9	,	0	],
-    [	0	,	1	,	0	,	0	,	0	,	0	,	0	,	0	,	0	],
-    [	5	,	0	,	1	,	0	,	0	,	7	,	9	,	0	,	0	],
-    [	0	,	6	,	0	,	0	,	5	,	0	,	0	,	0	,	0	],
-    [	0	,	4	,	0	,	0	,	0	,	0	,	0	,	1	,	0	] ]
-
-if (solve(grid)) {
-    for (let row = 0; row < grid.length; row++) {
-        print(row);
+function print(inputValues) {
+  const resultTable = document.getElementById("resultTable");
+  resultTable.innerHTML = "";
+  for (let i = 0; i < inputValues.length; i++) {
+    const resultRow = document.createElement("tr");
+    for (let j = 0; j < inputValues[i].length; j++) {
+      const resultCol = document.createElement("td");
+      resultCol.textContent = inputValues[i][j];
+      resultRow.appendChild(resultCol);
     }
-} else {
-    print("No solution exists.");
+    resultTable.appendChild(resultRow);
+  }
 }
